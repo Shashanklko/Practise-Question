9e1cf42dba9a54065a54067cf48fb8006385aee0
Shashank <139386976+Shashanklko@users.noreply.github.com>
2025-07-13 22:19:03 +0530

    Update person.js

--- Page/person.js
+++ Page/person.js
@@ -1,12 +1,29 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// const person1 = new Person("Alice", 25);
+// const person2 = new Person("Bob", 30);
+
+// console.log(person1.name);
+// console.log(person1.age);
+// console.log(person2.name);
+// console.log(person2.age);
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
 }
 
 const person1 = new Person("Alice", 25);
 const person2 = new Person("Bob", 30);
 
-console.log(person1.name);
-console.log(person1.age);
-console.log(person2.name);
-console.log(person2.age);
+person1.sayHello();
+person2.sayHello();
